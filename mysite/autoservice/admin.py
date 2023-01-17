e48d2a2ff8798f7b7a78ea9e3fc181337ca89618
Edvinas Jarmalavicius <edvinas400@gmail.com>
2023-01-17 14:17:24 +0200

    antros paskaitos uzduotys

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
 from . import models
 
+
+class UzsakymoEilutesInLine(admin.TabularInline):
+    model = models.UzsakymoEilute
+    extra = 0
+
+
+class UzsakymasAdmin(admin.ModelAdmin):
+    list_display = ("automobilis", "data")
+    inlines = [UzsakymoEilutesInLine]
+
+
+class AutomobilisAdmin(admin.ModelAdmin):
+    list_display = ("klientas", "automobilis", "valstybinis_nr", "vin_kodas")
+    list_filter = ("klientas", "automobilis")
+    search_fields = ("valstybinis_nr", "vin_kodas")
+
+
+class UzsakymoEiluteAdmin(admin.ModelAdmin):
+    list_display = ("paslauga", "kiekis")
+
+
+class PaslaugaAdmin(admin.ModelAdmin):
+    list_display = ("pavadinimas", "kaina")
+
+
 # Register your models here.
 admin.site.register(models.AutomobilioModelis)
-admin.site.register(models.Automobilis)
-admin.site.register(models.Uzsakymas)
-admin.site.register(models.Paslauga)
-admin.site.register(models.UzsakymoEilute)
+admin.site.register(models.Automobilis, AutomobilisAdmin)
+admin.site.register(models.Uzsakymas, UzsakymasAdmin)
+admin.site.register(models.Paslauga, PaslaugaAdmin)
+admin.site.register(models.UzsakymoEilute, UzsakymoEiluteAdmin)
--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -10,15 +10,23 @@ class AutomobilioModelis(models.Model):
     def __str__(self):
         return f"{self.marke} {self.modelis}"
 
+    class Meta:
+        verbose_name = "Automobilio modelis"
+        verbose_name_plural = "Automobilio modeliai"
+
 
 class Automobilis(models.Model):
     valstybinis_nr = models.CharField("Valstybinis numeris", max_length=200, help_text="Valstybinis numeris")
-    modelis = models.ForeignKey("AutomobilioModelis", on_delete=models.SET_NULL, null=True)
+    automobilis = models.ForeignKey("AutomobilioModelis", on_delete=models.SET_NULL, null=True)
     vin_kodas = models.CharField("VIN kodas", max_length=200, help_text="VIN kodas")
     klientas = models.CharField("Klientas", max_length=200, help_text="Klientas")
 
     def __str__(self):
-        return f"{self.modelis} {self.modelis}"
+        return f"{self.automobilis} {self.valstybinis_nr}"
+
+    class Meta:
+        verbose_name = "Automobilis"
+        verbose_name_plural = "Automobiliai"
 
 
 class Uzsakymas(models.Model):
@@ -28,19 +36,31 @@ class Uzsakymas(models.Model):
     def __str__(self):
         return f"{self.automobilis} {self.data}"
 
+    class Meta:
+        verbose_name = "Uzsakymas"
+        verbose_name_plural = "Uzsakymai"
+
 
 class Paslauga(models.Model):
     pavadinimas = models.CharField("Pavadinimas", max_length=200, help_text="Paslaugos pavadinimas")
-    kaina = models.FloatField("Kaina")
+    kaina = models.DecimalField("Kaina", decimal_places=2, max_digits=100)
 
     def __str__(self):
         return f"{self.pavadinimas}"
 
+    class Meta:
+        verbose_name = "Paslauga"
+        verbose_name_plural = "Paslaugos"
+
 
 class UzsakymoEilute(models.Model):
     paslauga = models.ForeignKey("Paslauga", on_delete=models.CASCADE)
-    uzsakymas = models.ForeignKey("Uzsakymas", on_delete=models.CASCADE)
+    uzsakymas = models.ForeignKey("Uzsakymas", on_delete=models.CASCADE, related_name="eilute")
     kiekis = models.IntegerField("Kiekis", help_text="Kiekis")
 
     def __str__(self):
         return f"{self.uzsakymas} {self.paslauga}"
+
+    class Meta:
+        verbose_name = "Uzsakymo eilute"
+        verbose_name_plural = "Uzsakymo eilutes"
